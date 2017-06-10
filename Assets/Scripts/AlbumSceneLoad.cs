6c9b18ba2fe1a3173ff8aca611848232b33bcd1d
sakuriver <suzue@yahoo.co.jp>
2017-06-10 15:13:44 +0900

    アルバム画面のデザインが来たので、仮の遷移を作成中

---
+++ Assets/Scripts/AlbumSceneLoad.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class AlbumSceneLoad : MonoBehaviour
+{
+
+    public SePlayer sePlayer;
+
+
+    public void OnClick()
+    {
+        sePlayer.onClickSe();
+        StartCoroutine("AlbumScene");
+    }
+
+    IEnumerator AlbumScene()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("album");
+    }
+}
